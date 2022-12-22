3d21df179b60f0e10347f59633abfe8ffce02cb9
Hauxy17 <ella.costello@yahoo.com>
2022-12-22 14:10:07 +0100

    Added: Sout Comment Object of Person.java Person.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("This has been added in local branch");
+        // This is to show that a new file has been added in local branch and that files code can be used in main
+        Person person = new Person("Main Person");
+        System.out.println(person);
     }
 }
---
+++ src/Person.java
@@ -0,0 +1,7 @@
+public class Person {
+    String forUseInMain;
+
+    public Person(String forUseInMain) {
+        this.forUseInMain = forUseInMain;
+    }
+}
